6726e0f646017dfbd5de1561f6e334776f2c9e2b
Lucas Flores <gian.flores@davinci.edu.ar>
2024-09-30 11:39:48 -0300

    clase arreglad

--- Automovil.py
+++
@@ -1,9 +0,0 @@
-class Automovil:
-    ruedas = 4
-    def __init__(color,marca,aceleracion,velocidad):
-        self.color = color
-        self.marca = marca
-        self.aceleracion = aceleracion
-        self.velocidad = velocidad
-
-    

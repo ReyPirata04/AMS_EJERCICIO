04c9a641110efe47af5c3f377ee393c66425a611
Lucas Flores <gian.flores@davinci.edu.ar>
2024-10-21 16:16:55 -0300

    Metodo Abstracto Conducir Agregado

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
+from abc import ABC, abstractmethod
+
 #Clase Automóvil con su atributo y metodos
-class Automovil:
+class Automovil(ABC):
     ruedas = 4
     def __init__(self,color,marca,aceleracion,velocidad):
         self.color = color
         self.marca = marca
         self.aceleracion = aceleracion
         self.velocidad = velocidad
+
+    @abstractmethod
+    def conducir(self):
+        pass
 
     def acelerar(self):
         self.velocidad += self.aceleracion
